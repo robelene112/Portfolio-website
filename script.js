3466bf37ecdedc4905a7123624911765e248b49c
David Engelen <robelene112@gmail.com>
2022-12-06 21:29:42 +0100

    Start implementing lazy-load

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+
+// Set up the IntersectionObserver
+const observer = new IntersectionObserver((entries) => {
+  // For each entry that intersects with the viewport
+  entries.forEach((entry) => {
+    entry.target.classList.toggle("show", entry.isIntersecting);
+  });
+});
+
+// Select all elements that should be lazy loaded
+const lazyLoadElements = document.querySelectorAll(".lazy-load");
+
+// Start observing each lazy load element
+lazyLoadElements.forEach((element) => observer.observe(element));
